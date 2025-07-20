c8f55faf259f9189a0f463dc957471489cb9eed3
Prachi aggarwal <PRACHI.AGGARWAL3838@GMAIL.COM>
2025-07-20 23:47:37 +0530

    test fixes

--- Notification-service/src/test/java/com/smartFleetManagementSystem/NotificationService/NotificationServiceApplicationTests.java
+++ Notification-service/src/test/java/com/smartFleetManagementSystem/NotificationService/NotificationServiceApplicationTests.java
@@ -2,10 +2,7 @@ package com.smartFleetManagementSystem.NotificationService;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.kafka.core.KafkaTemplate;
 
-import com.smartFleetManagementSystem.NotificationService.event.NotificationEvent;
 
 @SpringBootTest
 class NotificationServiceApplicationTests {
