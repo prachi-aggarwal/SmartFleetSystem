71986c9f157d437dd44c8b107138d04a85a5c87e
Prachi aggarwal <PRACHI.AGGARWAL3838@GMAIL.COM>
2025-07-20 15:36:12 +0530

    fleet service server changes

--- fleet-service/src/main/java/com/smartFleetManagementSystem/Fleet_service/FleetServiceApplication.java
+++ fleet-service/src/main/java/com/smartFleetManagementSystem/Fleet_service/FleetServiceApplication.java
@@ -2,8 +2,11 @@ package com.smartFleetManagementSystem.Fleet_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-
+@EnableRetry  // For automatic retries
+@EnableScheduling  // For connection cleanup tasks
 @SpringBootApplication 
 public class FleetServiceApplication {
 
--- fleet-service/src/main/java/com/smartFleetManagementSystem/Fleet_service/fleetService/vehicleService.java
+++ fleet-service/src/main/java/com/smartFleetManagementSystem/Fleet_service/fleetService/vehicleService.java
@@ -1,4 +1,5 @@
 package com.smartFleetManagementSystem.Fleet_service.fleetService;
+import java.time.LocalDateTime;
 import java.util.*;
 import org.springframework.stereotype.Service;
 
@@ -9,6 +10,7 @@ import com.smartFleetManagementSystem.Fleet_service.Repository.vehicleRepository
 @Service
 public class vehicleService {
     private final vehicleRepository vehicleRepository;
+
     public vehicleService(vehicleRepository vehicleRepository) {
     	this.vehicleRepository=vehicleRepository;
     }
@@ -22,25 +24,25 @@ public class vehicleService {
     	    return response;
     }
 	public Vehicle getFleetById(Long id) {
-		// TODO Auto-generated method stub
 	 Vehicle vehicles= vehicleRepository.findById(id).orElseThrow(()->new RuntimeException("id not found"));
 		return vehicles;
 	}
 	public Vehicle addVehicle(Vehicle vehicle) {
-		// TODO Auto-generated method stub
-		return vehicleRepository.save(vehicle) ;
+		Vehicle saved = vehicleRepository.save(vehicle);
+
+            return saved;
 	}
 	public Vehicle updateVehicle(Long id, Vehicle updatedVehicle) {
 	    Vehicle existingVehicle = vehicleRepository.findById(id)
 	        .orElseThrow(() -> new RuntimeException("Vehicle not found with id: " + id));
 
-	    // Update fields manually
+
 	    existingVehicle.setModel(updatedVehicle.getModel());
 	    existingVehicle.setPlateNumber(updatedVehicle.getPlateNumber());
 	    existingVehicle.setCapacity(updatedVehicle.getCapacity());
 	    existingVehicle.setActive(updatedVehicle.isActive());
 
-	    // Save updated entity
+	
 	    return vehicleRepository.save(existingVehicle);
 	}
 	public void deleteVehicleById(Long id) {
@@ -49,4 +51,5 @@ public class vehicleService {
         }
         vehicleRepository.deleteById(id);
     }
+	  
 }
